fbd75f86ef14dc155db5f10d6b2d3ec85f1da060
karchaoui Abdessamad <140338304+abdok10@users.noreply.github.com>
2024-06-02 19:57:46 +0100

    some production bugging

--- app/products/page.js
+++ app/products/page.js
@@ -1,11 +1,13 @@
-import ProductCard from "@/app/components/ProductCard";
+// import ProductCard from "@/app/components/ProductCard";
+
+import Productcard from "../components/Productcard";
 
 function ProductsPage() {
   return (
     <div className="mt-12">
       <p className="text-xl font-bold text-cyan-300">Products Page:</p>
       <div className="grid items-start justify-center md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
-        <ProductCard />
+        <Productcard />
       </div>
     </div>
   );
